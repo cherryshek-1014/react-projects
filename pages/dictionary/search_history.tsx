740ed0315750690083c90cdf58ffef9cd25e3625
Cherry Shek <cherryshek.1014@gmail.com>
2023-04-05 12:20:50 +0100

    move const to seperate files

--- pages/dictionary/search_history.tsx
+++ pages/dictionary/search_history.tsx
@@ -3,10 +3,22 @@ import styles from '>>/styles/dictionary.module.css'
 
 
 type Props = {
-    searchWord: Map<string, number>
+    setDisplayText: React.Dispatch<React.SetStateAction<string>>
+    searchList: String[]
 }
 
-const History = ({ searchWord }: Props) => {
+const History = ({ setDisplayText, searchList }: Props) => {
+    const jumpTo = (word) => {
+        setDisplayText(word);
+    }
+
+    const searchWord = searchList.map((searchText, index) => {
+        return (
+            <li key={index}>
+                <button onClick={() => jumpTo(searchText)}>{searchText}</button>
+            </li>
+        )
+    })
     return (
         <div className={styles["history-col"]}>
             <p> Previous Searches </p>
